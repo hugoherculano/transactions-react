60a14f0bb26163ef81d0272fa3b5885170265759
hugoherculano <hugohercullano@gmail.com>
2023-05-10 16:20:03 -0300

    struct frontend completed! :rocket:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from '../../assets/logo.svg';
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -8,7 +10,14 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SummaryContainer = styled.section`
   width: 100%;
@@ -13,8 +13,29 @@ export const SummaryContainer = styled.section`
   margin-top: -5rem;
 `;
 
-export const SummaryCard = styled.div`
+interface SummaryCardProps {
+  variant?: 'green';
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
   background-color: ${props => props.theme['gray-600']};
   border-radius: 6px;
   padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${props => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${props => props.variant === 'green' && css`
+    background-color: ${props.theme['green-700']};
+  `}
 `;
